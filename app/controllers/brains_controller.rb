d1f138df7cf9f8abf996b92ad9e1ee9f3af4d884
Celine Pineau <pineau.celine@gmail.com>
2024-03-12 15:59:56 +0100

    brains_index

--- app/controllers/brains_controller.rb
+++ app/controllers/brains_controller.rb
@@ -1,9 +1,10 @@
 class BrainsController < ApplicationController
+  def index
+    @brains = Brain.all
+  end
 
   def show
     id = params[:id]
     @brain = Brain.find(params[:id])
   end
-
-
 end
