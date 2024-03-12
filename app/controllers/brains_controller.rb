7a80713d0b3c088585ea88172765f31f8834628d
Lea Seica <lea.seica@skema.edu>
2024-03-12 14:52:49 +0100

    controller brains with new and create

--- app/controllers/brains_controller.rb
+++ app/controllers/brains_controller.rb
@@ -1,2 +1,22 @@
 class BrainsController < ApplicationController
+
+  def new
+    @brain = Brain.new
+  end
+
+  def create
+    @brain = Brain.new(brain_params)
+    if @brain.save
+      redirect_to brains_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def brain_params
+    params.require(:brain).permit(:title, :specialty, :address, :photo, :description, :wiki_page, :price)
+  end
+
 end
