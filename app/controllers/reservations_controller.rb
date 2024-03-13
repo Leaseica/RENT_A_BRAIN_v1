35c38efe7abd9ed23eee096069473bccca60f486
Olivier Rizk <oliv.rizk@gmail.com>
2024-03-13 15:29:22 +0100

    coder accept et decline dans controller reservations

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,13 @@
 class ReservationsController < ApplicationController
+  def accept
+    @reservation = Reservation.find(params[:id])
+    @reservation.status = true
+    @reservation.save
+  end
+
+  def decline
+    @reservation = Reservation.find(params[:id])
+    @reservation.destroy
+    redirect_to profile_path(current_user)
+  end
 end
