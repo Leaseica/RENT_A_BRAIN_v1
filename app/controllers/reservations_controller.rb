34911ab1f5effd291c091deab5d50e328f480600
Lea Seica <lea.seica@skema.edu>
2024-03-13 13:10:42 +0100

    added list of my brains in new form, updated flatpicker for brains and reservations, method new and create for reservation and form. Cost is added manually for now

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,33 @@
 class ReservationsController < ApplicationController
+
+  def show
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def new
+    @brain = Brain.find(params[:brain_id])
+    @reservation = Reservation.new
+    @reservation.user_id = current_user.id
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+    @reservation.user_id = current_user.id
+    @reservation.brain_id = params[:brain_id]
+    respond_to do |format|
+      if @reservation.save!
+        format.html { redirect_to reservation_path(@reservation) }
+        format.json
+      else
+        format.html { render "reservations/new", status: :unprocessable_entity }
+        format.json
+      end
+    end
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:start_date, :end_date, :cost)
+  end
 end
